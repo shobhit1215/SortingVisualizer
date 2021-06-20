5f0eb144e788096ae7d49a0a41cb9580ab48e7aa
Shobhit <imshobhit.sb@gmail.com>
2021-06-20 22:50:14 +0530

    Started Project

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+import random
+
+root = Tk()
+root.title('Sorting Algorithms Visualization')
+root.geometry("1000x1000")
+root.maxsize(1500,1500)
+root.config(bg='black')
+
+
+
+root.mainloop()
