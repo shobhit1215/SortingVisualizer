049440a3babc07f92002a420ebacc3a378099133
Shobhit <imshobhit.sb@gmail.com>
2021-06-21 10:55:41 +0530

    Created Canvas

--- main.py
+++ main.py
@@ -9,8 +9,50 @@ root.config(bg='black')
 
 select_alg = StringVar()
 
+def drawData(data):
+    canvas.delete("all")
+    c_height = 500
+    c_width = 900
+    x_width = c_width/(len(data)+1)
+    offset= 30
+    spacing=10
+    normalizeddata = [i/max(data) for i in data]
+
+    for i, height in enumerate(normalizeddata):
+        x0=i*x_width +offset +spacing
+        y0=c_height - height * 340
+
+        x1=(i+1)*x_width + offset
+        y1=c_height
+
+        canvas.create_rectangle(x0,y0,x1,y1,fill="red")
+        canvas.create_text(x0+2,y0,anchor=SW,text=str(data[i]))
+
+
+
 def generate():
     print("Selected Algorithm" + select_alg.get())
+    try:
+        minValue = int(minEntry.get())
+    except:
+        minValue = 1
+    try:
+        maxValue = int(maxEntry.get())
+    except:
+        maxValue =100
+    try:
+        size = int(sizeEntry.get())
+    except:
+        size =20
+    if minValue<0 : minValue=0
+    if maxValue > 1000 : maxValue=1000
+    if size > 50 : size=50
+    if size <3 :size=3
+    if minValue > maxValue : minValue,maxValue=maxValue,minValue
+    data = []
+    for _ in range(size):
+        data.append(random.randrange(minValue,maxValue+1))
+    drawData(data)
 
 UI_Frame = Frame(root,width=900,height=200,bg='grey')
 UI_Frame.grid(row=0,column=0,padx=10,pady=5)
